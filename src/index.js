06417fccf6a68719e3e8404c549c0ae30654895a
irina <irina.konovalova455@gmail.com>
2021-01-15 12:37:08 +0200

    deleted script tags to get rid of load error

--- src/index.js
+++ src/index.js
@@ -5,25 +5,11 @@ const menuCard = document.querySelector('#card1');
 const languageBtn = document.createElement('button');
 languageBtn.classList.add('language-btn');
 languageBtn.innerHTML = 'FI/EN';
-/*
-const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-                "Goalaista kalacurrya ja täysjyväriisiä",
-                "vegaani Chili sin carne ja täysjyväriisi",
-                "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-                "Lunch baguette with BBQ-turkey filling",
-                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
-
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
-                "Goan style fish curry and whole grain rice",
-                "Vegan Chili sin carne and whole grain rice",
-                "Broccoli puree soup, side salad with two napas",
-                "Lunch baguette with BBQ-turkey filling",
-                "Cheese / Chicken / Vege / Halloum burger and french fries"];*/
 
 let coursesFi = [];
 let coursesEn = [];
 
-for(let i = 1; i <= Object.keys(menuData.courses).length; i++) {
+for(let i = 1; i <= Object.values(menuData.courses).length; i++) {
   coursesFi.push(menuData.courses[i].title_fi);
   coursesEn.push(menuData.courses[i].title_en);
 }
