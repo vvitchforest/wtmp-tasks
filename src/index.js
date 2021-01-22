94f64787c4cf41e25a8700919263bd2a3ec08d32
irina <irina.konovalova455@gmail.com>
2021-01-22 14:07:50 +0200

    trying to solve task1

--- src/index.js
+++ src/index.js
@@ -2,97 +2,22 @@ const min = 1;
 const max = 100;
 const maxGuess = 10;
 
-let randomNumber = Math.floor(Math.random() * max) + min;
-
-const resultParas = document.querySelector('.resultParas');
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
-
+let randNum = Math.floor(Math.random() * max) + min;
 let guessCount = 1;
-let resetButton;
+let guess = Math.ceil((max-min)/2);
+let guessArray = [];
+guessArray.push(0);
 
-let start = 0;
-let timeToGuess = 0;
-
-const checkGuess = () => {
-  let userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    start = Date.now();
-    console.log('starting timer...');
-    guesses.textContent = 'Previous guesses: ';
+while(guessCount <= 10 && guess !== randNum) {
+  if(randNum > guess) {
+    guess = guess + (guess - guessArray[guessArray.length-1])/2;
+  } else if (randNum < guess) {
+    guess = guess - (guess - guessArray[guessArray.length-1])/2;
   }
-  guesses.textContent += userGuess + ' ';
-
-  if (userGuess === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-
-    let resultSummary = document.createElement('div');
-    let totalGuesses = document.createElement('p');
-    let totalTime = document.createElement('p');
-
-    totalGuesses.innerHTML = 'Your total guess count is ' + guessCount;
-    totalTime.innerHTML = 'Your total guess time was ' + Math.floor(timeToGuess / 1000) + ' seconds';
-
-    resultSummary.appendChild(totalGuesses);
-    resultSummary.appendChild(totalTime);
-    resultParas.appendChild(resultSummary);
-
-    resultSummary.style.backgroundColor = 'lightgrey';
-
-  } else if (guessCount === maxGuess) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    setGameOver();
-  } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    if(userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if(userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
-    }
-  }
-
+  guessArray.push(guess);
   guessCount++;
-  guessField.value = '';
-  guessField.focus();
-}
+};
 
-guessSubmit.addEventListener('click', checkGuess);
+console.log(randNum);
+console.log(guessArray);
 
-const setGameOver = () => {
-  timeToGuess = Date.now() - start;
-  console.log(timeToGuess);
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
-}
-
-const resetGame = () => {
-  guessCount = 1;
-
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (let i = 0 ; i < resetParas.length ; i++) {
-    resetParas[i].textContent = '';
-  }
-
-  resetButton.parentNode.removeChild(resetButton);
-
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
-
-  lastResult.style.backgroundColor = 'white';
-
-  randomNumber = Math.floor(Math.random() * max) + min;
-}
