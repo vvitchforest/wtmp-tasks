5f7251e5e94de6941480394d39fca7f582a9b840
irina <irina.konovalova455@gmail.com>
2021-01-24 20:14:26 +0200

    week2-task1 completed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
-const min = 1;
-const max = 100;
-const maxGuess = 10;
+/*Strategy for finding the random number is to split the search range in half each time search is done */
 
-let randNum = Math.floor(Math.random() * max) + min;
-let guessCount = 1;
-let guess = Math.ceil((max-min)/2);
-let guessArray = [];
-guessArray.push(0);
+guessNumber = () => {
+  let min = 1;
+  let max = 100;
+  let randNum = Math.floor(Math.random() * max) + min;
+  let guessHistory = [];
 
-while(guessCount <= 10 && guess !== randNum) {
-  if(randNum > guess) {
-    guess = guess + (guess - guessArray[guessArray.length-1])/2;
-  } else if (randNum < guess) {
-    guess = guess - (guess - guessArray[guessArray.length-1])/2;
+  //Loops until the random number is found
+  while (true) {
+
+    /*Guess is always based on min and max boundaries, whis are updated with each iteration of loop */
+    let guess = Math.floor((min + max) / 2);
+
+    guessHistory.push(guess);
+    //Breaks when the correct number is found
+    if (randNum === guess) {
+      console.log('Random number is correct! ' + guess);
+      break;
+    }
+    /*If guess is smaller than random number, guess+1 becomes current minimum boundary
+    (lower numbers are no longer guessed)*/
+    if (randNum > guess) {
+      console.log('Greater than ' + guess);
+      min = guess + 1;
+    }
+    /*Ig guess is greater than the random number, guess-1 becomes a new maximum boundary,
+    and higher numbers are no longer guessed */
+    if (randNum < guess) {
+      console.log('Smaller than ' + guess);
+      max = guess - 1;
+    }
+    console.log('next loop');
   }
-  guessArray.push(guess);
-  guessCount++;
+  console.log("Guess history: " + guessHistory);
+  console.log("Total number of guesses: " + guessHistory.length);
+  return guessHistory;
 };
 
-console.log(randNum);
-console.log(guessArray);
-
+//Testing 1500 times
+const iterations = 1500;
+let totalGuesses = 0;
+let maxGuessCount = 0;
+let minGuessCount = 10;
+for (let i = 0; i < 1500; i++) {
+  let currentGuessCount = guessNumber().length;
+  totalGuesses += currentGuessCount;
+  if(currentGuessCount > maxGuessCount) {
+    maxGuessCount = currentGuessCount;
+  }
+  if(currentGuessCount < minGuessCount) {
+    minGuessCount = currentGuessCount;
+  }
+};
+//Average guesses
+let averageGuesses = (totalGuesses / iterations).toFixed(2);
+console.log("Average number of total guess count: " + averageGuesses);
+console.log('Most guesses ' + maxGuessCount);
+console.log('Least guesses ' + minGuessCount);
