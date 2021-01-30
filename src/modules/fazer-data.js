a1a2be8b5d9d7a57f4be523f6737f999824b6285
irina <irina.konovalova455@gmail.com>
2021-01-30 13:43:57 +0200

    adapter pattern refactored

--- src/modules/fazer-data.js
+++ src/modules/fazer-data.js
@@ -1,59 +1,35 @@
 import FazerMenuFi from '../assets/fazer-menu-fi.json';
 import FazerMenuEn from '../assets/fazer-menu-en.json';
 
-console.log(FazerMenuEn);
 let coursesEn = [];
 let coursesFi = [];
 
-const parseFazerMenuFi = (menuFi) => {
-  let mealsFi = [];
-  menuFi.LunchMenus[0].SetMenus.forEach((setMenu) => {
-    mealsFi.push(setMenu);
+const parseMenu = (menuData) => {
+  let meals = [];
+  menuData.LunchMenus[0].SetMenus.forEach((setMenu) => {
+    meals.push(setMenu);
   });
-  return mealsFi;
+  return meals;
 };
 
-const parseFazerMenuEn = (menuEn) => {
-  let mealsEn = [];
-  menuEn.LunchMenus[0].SetMenus.forEach((setMenu) => {
-    mealsEn.push(setMenu);
-  });
-  return mealsEn;
+const joinMeals = (parsedMenu, coursesArray) => {
+parsedMenu.forEach((setMenu) => {
+  const meals = setMenu.Meals.map(x => x.Name).join(", ");
+  coursesArray.push(meals);
+});
 };
 
+const parsedFi = parseMenu(FazerMenuFi);
+const parsedEn = parseMenu(FazerMenuEn);
+joinMeals(parsedFi, coursesFi);
+joinMeals(parsedEn, coursesEn);
 
-const joinMealsFi = (parsedMenu, coursesTarget) => {
-
-  parsedMenu.forEach((setMenu) => {
-    let mealString = "";
-    setMenu.Meals.forEach((meal) => {
-      mealString += meal.Name + ", ";
-    });
-    mealString = mealString.slice(0, (mealString.length - 2));
-    if (coursesTarget === 'fin') {
-      coursesFi.push(mealString);
-    } else if (coursesTarget === 'eng') {
-      coursesEn.push(mealString);
-    }
-  });
-};
-
-const parsedFi = parseFazerMenuFi(FazerMenuFi);
-const parsedEn = parseFazerMenuEn(FazerMenuEn);
-joinMealsFi(parsedFi, 'fin');
-joinMealsFi(parsedEn, 'eng');
-
-
-const FazerDataFi = { coursesFi };
-const FazerDataEn = { coursesEn };
+const FazerDataFi = {coursesFi};
+const FazerDataEn = {coursesEn};
 
 const FazerData = {
   FazerDataFi: FazerDataFi,
   FazerDataEn: FazerDataEn,
-
 };
 
 export default FazerData;
-
-
-
