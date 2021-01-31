b314b6e4a6b7067ccdba525875e2988a0684b20e
irina <irina.konovalova455@gmail.com>
2021-01-31 13:43:06 +0200

    started task1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello console!');
+//TASK 1 i.
+
+const createSecretCode = (secretCode) => {
+
+  document.addEventListener('keypress', event => {
+
+
+
+  });
+
+};
+createSecretCode('hello');
